776b885ffa0886d5b23e8d4d3ce56fba02b15abe
dogata <dogata07@gmail.com>
2021-12-21 23:46:18 -0700

    1 file modified: Adding overlay to charts

--- Chart.tsx
+++ Chart.tsx
@@ -21,6 +21,25 @@ class GanttChart extends Component {
       .taskStatus(taskStatus)
       .tickFormat(tickFormat);
     gantt(tasks);
+    const svg = d3.select('.chart'); // select plot area
+    const data = [12, 5, 6, 6, 9, 10];
+    svg
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('line')
+      .attr('x', this.props.width / 2)
+      .attr('y', this.props.height);
+    svg
+      .selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * 70)
+      .attr('y', (d, i) => this.props.height - 10 * d)
+      .attr('width', 65)
+      .attr('height', (d, i) => d * 10)
+      .attr('fill', 'green');
   }
 
   render() {

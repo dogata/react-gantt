88c3ac3ccf12dc48f52e2b61b55141f9cc7c6827
dogata <dogata07@gmail.com>
2021-12-21 22:02:59 -0700

    3 files modified: Load input data

--- index.tsx
+++ index.tsx
@@ -2,62 +2,20 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import GanttChart from './Chart';
 import './style.css';
+import Tasks from './tasks.json';
+
+const parseDates = (s: string): Date => {
+  //var [YYYY, MM, DD] = '2014-04-03'.split('-');
+  return new Date(s + 'T00:00:00');
+};
 
 /* Overlay precedence towards the end of list */
 const tasks = [
   {
-    startDate: new Date('Sat Dec 08 21:00:00 EST 2012'),
-    endDate: new Date('Sat Dec 08 24:00:00 EST 2012'),
-    taskName: 'ModelA',
-    status: 'RUNNING',
-  },
-  {
-    startDate: new Date('Sun Dec 09 04:56:32 EST 2012'),
-    endDate: new Date('Sun Dec 09 10:35:47 EST 2012'),
-    taskName: 'ModelA',
-    status: 'RUNNING',
-  },
-  {
-    startDate: new Date('Sun Dec 09 04:02:45 EST 2012'),
-    endDate: new Date('Sun Dec 09 04:48:56 EST 2012'),
-    taskName: 'ModelB',
-    status: 'RUNNING',
-  },
-  {
-    startDate: new Date('Sun Dec 09 01:40:11 EST 2012'),
-    endDate: new Date('Sun Dec 09 03:26:35 EST 2012'),
-    taskName: 'ModelA',
-    status: 'SUCCEEDED',
-  },
-  {
-    startDate: new Date('Sun Dec 09 01:08:42 EST 2012'),
-    endDate: new Date('Sun Dec 09 01:33:42 EST 2012'),
-    taskName: 'ModelB',
-    status: 'FAILED',
-  },
-  {
-    startDate: new Date('Sun Dec 09 09:02:45 EST 2012'),
-    endDate: new Date('Sun Dec 09 09:48:56 EST 2012'),
-    taskName: 'ModelB',
-    status: 'RUNNING',
-  },
-  {
-    startDate: new Date('Sun Dec 09 08:40:11 EST 2012'),
-    endDate: new Date('Sun Dec 09 08:46:35 EST 2012'),
-    taskName: 'ModelA',
-    status: 'SUCCEEDED',
-  },
-  {
-    startDate: new Date('Sun Dec 09 11:08:42 EST 2012'),
-    endDate: new Date('Sun Dec 09 11:33:42 EST 2012'),
-    taskName: 'ModelB',
-    status: 'FAILED',
-  },
-  {
-    startDate: new Date('Sat Dec 08 23:12:24 EST 2012'),
-    endDate: new Date('Sun Dec 09 00:26:13 EST 2012'),
-    taskName: 'ModelA',
-    status: 'KILLED',
+    startDate: parseDates(Tasks[0].startDate),
+    endDate: parseDates(Tasks[0].endDate),
+    taskName: Tasks[0].taskName,
+    status: "PLANNED"
   },
 ];
 
@@ -66,6 +24,7 @@ const taskStatus = {
   FAILED: 'bar-failed',
   RUNNING: 'bar-running',
   KILLED: 'bar-killed',
+  PLANNED: 'bar-planned',
 };
 
 const taskNames = ['ModelA', 'ModelB'];
@@ -79,8 +38,3 @@ render(
   />,
   document.getElementById('root')
 );
-
-const parseDates = (s: string): Date => {
-  //var [YYYY, MM, DD] = '2014-04-03'.split('-');
-  return new Date(s+"T00:00:00");
-}
